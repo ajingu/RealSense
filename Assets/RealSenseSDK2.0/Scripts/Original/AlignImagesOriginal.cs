58cb1155a770ed23be610396833df32185ee58ca
Arata Jingu <guujin0120@gmail.com>
2018-02-24 13:21:29 +0900

    First Commit.

---
+++ Assets/RealSenseSDK2.0/Scripts/Original/AlignImagesOriginal.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using Intel.RealSense;
+using System.Collections.Generic;
+
+public class AlignImagesOriginal : MonoBehaviour
+{
+
+    Align aligner;
+    public RealsenseStreamTextureOriginal from;
+    public RealsenseStreamTextureOriginal to;
+    public RealsenseDeviceOriginal realSenseDeviceOriginal;
+
+    // Use this for initialization
+    void Start()
+    {
+        aligner = new Align(Stream.Color);
+        realSenseDeviceOriginal.Instance.onNewSampleSet += OnFrameSet;
+    }
+
+    private void OnFrameSet(FrameSet frames)
+    {
+        using (var aligned = aligner.Process(frames))
+        {
+            from.OnFrame(aligned.DepthFrame);
+            to.OnFrame(aligned.ColorFrame);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO:
+    }
+}
